cc1065eaa46c2b5a90b3c57cb8aecfc82908de2c
tw5033 <tw5033@gmail.com>
2019-05-02 07:33:31 +0000

    Finish recursion for summing nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let sum = 0;
+  for(let item of array) {
+    if(Array.isArray(item)) {
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
 }
 
 module.exports = sumItems;
